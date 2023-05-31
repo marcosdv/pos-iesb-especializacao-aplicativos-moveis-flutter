d6a157a88fc7a1616aac0711cb02a36ecf31e833
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-05-30 22:56:44 -0300

    Delete aula_03_banco_dados directory

--- aula_03_banco_dados/lib/extensions/string_extension.dart
+++
@@ -1,9 +0,0 @@
-extension StringExtension on String {
-  int toInt({int valorPadrao = 0}) {
-    return int.tryParse(this) ?? valorPadrao;
-  }
-
-  double toDouble({double valorPadrao = 0}) {
-    return double.tryParse(this) ?? valorPadrao;
-  }
-}
